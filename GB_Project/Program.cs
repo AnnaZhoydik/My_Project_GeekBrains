a00eedf037c62571a94a45780744beac2b4b05a0
Anna <anka22@msn.com>
2023-02-25 00:22:30 +0300

    этап 4

---
+++ GB_Project/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = { "number", "im", "String", "ok", "!" };
+for (int i = 0; i < array.Length; i++)
+{
+    char[] a = array[i].ToCharArray();
+    char[] b = a.ToArray();
+    if (array[i].Length < 3)
+        Console.Write($"{array[i]} ");
+}
